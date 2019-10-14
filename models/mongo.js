69d8afe3bcca8c3694a20a1f5f0e2912e1c69ed5
UnsinkableSam <maraphostv@gmail.com>
2019-10-14 14:43:52 +0200

    Mongo update

--- models/mongo.js
+++ models/mongo.js
@@ -1,47 +1,14 @@
 "use strict";
 const mongo = require("mongodb").MongoClient;
-const dsn = process.env.DBWEBB_DSN || "mongodb://localhost:27017/chat";
+const dsn = "mongodb://localhost:27017/chat";
 const colName = "chat";
 
 
 module.exports =  {
-    /**
-     * Connect to the database and search using a criteria.
-     */
-    
-
-    
 
 
 
 
-
-
-
-    /**
-     * Add informaiton to database.
-     * 
-     *
-     * @async
-     *
-     * @param {string} dsn     DSN to connect to database.
-     * @param {string} colName Name of collection.
-     * @param {string} doc     Documents to be inserted into collection.
-     *
-     * @throws Error when database operation fails.
-     *
-     * @return {Promise<void>} Void
-     */
-    // async function addCollection(doc) {
-    //     const client = await mongo.connect(dsn);
-    //     const db = await client.db();
-    //     const col = await db.collection(colName);
-
-    //     await col.deleteMany();
-    //     await col.insertMany(doc);
-
-    //     await client.close();
-    // }
 
     addCollection: async function (doc) {
         console.log(doc);
